7392b963656052bf80ccd5cd897d339aa8bc3510
Peteys93 <ps293@email.vccs.edu>
2018-12-13 21:26:59 -0500

    Split game into separate variable for later use with UI

--- Program_Files/RankingsDateScraper.py
+++ Program_Files/RankingsDateScraper.py
@@ -4,14 +4,15 @@ import Funcs as scraper
 
 #change the value of high_points for upper bound, low_points for lower bound of points to scrape, change filename and level for filename and level to scrape from
 high_points, low_points = 100, 95
-filename = "temp.csv"
+
+game = "goldeneye" #or perfect-dark
 level = "surface1"
 
-#open file 'filename'
+filename = "temp.csv"
 f = scraper.openfile("\\out\\" + filename)
 
 #open chrome browser at 'url'
-driver = scraper.openbrowser("https://rankings.the-elite.net/goldeneye/stage/" + level)
+driver = scraper.openbrowser("https://rankings.the-elite.net/"+ game + "/stage/" + level)
 
 #parse xml on rankspage and timespage to get personal best and date info
 scraper.parse_xml(f, driver, high_points, low_points)
