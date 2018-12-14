233b6262b542c1ddfaa86228eef97455b18b4d8c
Peteys93 <ps293@email.vccs.edu>
2018-12-14 00:14:48 -0500

    Parse XML better for points, remove need for offsets, add difficulty selection, spacing changes

--- Program_Files/Funcs.py
+++ Program_Files/Funcs.py
@@ -19,86 +19,76 @@ def openbrowser(url):
     return driver
 
 #parse rankings page xml with bs4 to get Time, Points, then click on time link to get Date/Level/Name info from next page
-def parse_xml(f, driver, high_points, low_points):
+def parse_xml(f, driver, high_points, low_points, select_a, select_sa, select_00a):
     soup_level0=BeautifulSoup(driver.page_source, 'lxml')
 
-    #Offset by ten from top 10 leaders      
-    parse_points = soup_level0.find_all(class_="points")
-    points_string = str(parse_points)
-    points= re.findall("\d+", points_string)
+    #parse points
+    points = [p.text.strip() for p in soup_level0.select(".stage-table .points")]
 
     #parse agent, sa, and 00a times, store for later, add up lengths of the lists for later use as well    
     ts_agent = [a.text.strip() for a in soup_level0.select("#diff-0 .time")]
     ts_secret_agent = [b.text.strip() for b in soup_level0.select("#diff-1 .time")]
     ts_00_agent = [c.text.strip() for c in soup_level0.select("#diff-2 .time")]    
 
-    ag_times = len(ts_agent)
-    ag_sa_times = len(ts_agent) + len(ts_secret_agent) 
-    total_times = len(ts_agent) + len(ts_secret_agent) + len(ts_00_agent)
+    ag_times, ag_sa_times, total_times = len(ts_agent), len(ts_agent) * 2, len(ts_agent) * 3
 
     #loop through all times in agent, secret agent, and 00 agent lists
-    i, offset = 0, 0    
-    while i <= total_times:     
-        if(int(points[i]) <= high_points and int(points[i]) >= low_points): # change high_points and low_points in rankingsdatascraper.py for upper/lower bound        
-            if i < ag_times: # get link and time string for agent, set isa True for printing/writing
-                link_string = '//*[@id="diff-0"]/table/tbody/tr[' + str(i-offset+2) + ']/td[3]/a[1]' #offset by number of points leaders, +2 here because tr[2] is first item we want to click in each column
-                #ts_agent[i-offset] = re.sub(">", "", ts_agent[i-offset])
-                time_string = ts_agent            
-                isa, issa, is00a = True, False, False
-            elif i >= ag_times and i < ag_sa_times: # get link and time string for secret agent, update bools for print/write
-                link_string = '//*[@id="diff-1"]/table/tbody/tr[' + str(i-ag_times-offset+2) + ']/td[3]/a[1]'
-                #ts_secret_agent[i-ag_times-offset] = re.sub(">", "", ts_secret_agent[i-ag_times-offset])
-                time_string = ts_secret_agent            
-                isa, issa, is00a = False, True, False
-            elif i >= ag_sa_times and i < total_times: # get link and time string for 00agent, update bools for print/write
-                link_string = '//*[@id="diff-2"]/table/tbody/tr[' + str(i-ag_sa_times-offset+2) + ']/td[3]/a[1]'
-                #ts_00_agent[i-ag_sa_times-offset] = re.sub(">", "", ts_00_agent[i-ag_sa_times-offset])
-                time_string = ts_00_agent            
-                isa, issa, is00a = False, False, True
-            
+    i = 0
+    while i < total_times:     
+        if(int(points[i]) <= high_points and int(points[i]) >= low_points):
+            doClick = False #Dont click the link to parse the next page if loop doesn't go into any of these if statements
+            if i < ag_times and select_a: # get link and time string for agent, set isa True for printing/writing, first tr in times list is 2, set doClick true if link is set
+                link_string, time_string = '//*[@id="diff-0"]/table/tbody/tr[' + str(i+2) + ']/td[3]/a[1]', ts_agent        
+                isa, issa, is00a, doClick = True, False, False, True
+            elif (i >= ag_times and i < ag_sa_times) and select_sa: #secret agent
+                link_string, time_string = '//*[@id="diff-1"]/table/tbody/tr[' + str(i-ag_times+2) + ']/td[3]/a[1]', ts_secret_agent                          
+                isa, issa, is00a, doClick = False, True, False, True
+            elif (i >= ag_sa_times and i < total_times) and select_00a: #00/perfect agent
+                link_string, time_string = '//*[@id="diff-2"]/table/tbody/tr[' + str(i-ag_sa_times+2) + ']/td[3]/a[1]', ts_00_agent           
+                isa, issa, is00a, doClick = False, False, True, True
+                        
             #click the link given by the xpath for the link_string set above    
-            python_button = driver.find_element_by_xpath(link_string)
-            python_button.click()
+            if(doClick):
+                python_button = driver.find_element_by_xpath(link_string)
+                python_button.click()
 
-            #parse - times page xml with bs4 for to get Name, Date, and Level Name/Difficulty
-            soup_level1=BeautifulSoup(driver.page_source, 'lxml')
+                #parse - times page xml with bs4 for to get Name, Date, and Level Name/Difficulty
+                soup_level1=BeautifulSoup(driver.page_source, 'lxml')
 
-            #parse - zoom closer to name, level, and date on timespage, probably a better way to parse this for cleaner regex, but this works
-            parse_name = soup_level1.find_all(class_='user')
-            parse_level = soup_level1.find_all("h1")
-            parse_date = soup_level1.find_all("ul")            
+                #parse - zoom closer to name, level, and date on timespage, probably a better way to parse this for cleaner regex, but this works
+                parse_name = soup_level1.find_all(class_='user')
+                parse_level = soup_level1.find_all("h1")
+                parse_date = soup_level1.find_all("ul")            
             
-            name_string = str(parse_name)
-            level_string = str(parse_level)
-            date_string = str(parse_date)
+                name_string = str(parse_name)
+                level_string = str(parse_level)
+                date_string = str(parse_date)
 
-            ns2 = re.findall("[>][\w][A-Za-z\u00fc\-\s]*", name_string)
-            name_string_final = re.sub("[>]", "", str(ns2[0]))
+                ns2 = re.findall("[>][\w][A-Za-z\u00fc\-\s]*", name_string)
+                name_string_final = re.sub("[>]", "", str(ns2[0]))
 
-            ls2 = re.findall("[\t][A-Z][a-z]+[012A-Za-z\s\t]*[Agent\t]+", level_string)
-            ls3 = re.sub("[\t]", " ", str(ls2[0]))
-            level_string_final = ls3.strip()
+                ls2 = re.findall("[\t][A-Z][a-z]+[012A-Za-z\s\t]*[Agent\t]+", level_string)
+                ls3 = re.sub("[\t]", " ", str(ls2[0]))
+                level_string_final = ls3.strip()
                       
-            #check if date is listed, set to blank space if not listed.
-            if("<li><strong>Achieved:</strong> " in date_string):
-                date_string = date_string.split("<li><strong>Achieved:</strong> ")
-                date_string_final = date_string[1].split("</li>")
-            else:
-                date_string_final[0] = ""        
+                #check if date is listed, set to blank space if not listed.
+                if("<li><strong>Achieved:</strong> " in date_string):
+                    date_string = date_string.split("<li><strong>Achieved:</strong> ")
+                    date_string_final = date_string[1].split("</li>")
+                else:
+                    date_string_final[0] = ""        
         
-            #print to cmd and file, alter time string for a, sa, and 00a
-            if isa:
-                print(name_string_final, level_string_final, time_string[i-offset], points[i], date_string_final[0])
-                f.write(name_string_final + ',' + level_string_final + ',' + time_string[i-offset] + ',' +   points[i] + ',' + date_string_final[0] + '\n')
-            elif issa:
-                print(name_string_final, level_string_final, time_string[i-ag_times-offset], points[i], date_string_final[0])
-                f.write(name_string_final + ',' + level_string_final + ',' + time_string[i-ag_times-offset] + ',' +   points[i] + ',' + date_string_final[0] + '\n')
-            elif is00a:
-                print(name_string_final, level_string_final, time_string[i-ag_sa_times-offset], points[i], date_string_final[0])
-                f.write(name_string_final + ',' + level_string_final + ',' + time_string[i-ag_sa_times-offset] + ',' +   points[i] + ',' + date_string_final[0] + '\n')
+                #print to cmd and file, alter time string for a, sa, and 00a
+                if isa and select_a:
+                    print(name_string_final, level_string_final, time_string[i], points[i], date_string_final[0])
+                    f.write(name_string_final + ',' + level_string_final + ',' + time_string[i] + ',' +   points[i] + ',' + date_string_final[0] + '\n')
+                elif issa and select_sa:
+                    print(name_string_final, level_string_final, time_string[i-ag_times], points[i], date_string_final[0])
+                    f.write(name_string_final + ',' + level_string_final + ',' + time_string[i-ag_times] + ',' +   points[i] + ',' + date_string_final[0] + '\n')
+                elif is00a and select_00a:
+                    print(name_string_final, level_string_final, time_string[i-ag_sa_times], points[i], date_string_final[0])
+                    f.write(name_string_final + ',' + level_string_final + ',' + time_string[i-ag_sa_times] + ',' +   points[i] + ',' + date_string_final[0] + '\n')
 
-            #go back when finished parsing date/name/level info to allow the program to move onto the next link
-            driver.execute_script("window.history.go(-1)")
-        elif int(points[i]) > 100: # set the offset based on the number of points leaders at the top of the page (as they are read into the 'points' parse from earlier)
-            offset += 1
+                #go back when finished parsing date/name/level info to allow the program to move onto the next link
+                driver.execute_script("window.history.go(-1)")          
         i += 1
--- Program_Files/RankingsDateScraper.py
+++ Program_Files/RankingsDateScraper.py
@@ -1,21 +1,22 @@
 import Funcs as scraper
 
 #programs scrapes date information for chosen times on rankings.the-elite.net
+#change the value of high_points for upper bound, low_points for lower bound of points to scrape,
+#set select_* false to ignore times from that difficulty
+#change game and level for game and level to scrape from
+#change filename to desired .csv file
+high_points, low_points = 100, 95 
+select_a, select_sa, select_00a = True, True, True 
+game, level = "perfect-dark", "defection" 
+filename = "test2.csv" 
 
-#change the value of high_points for upper bound, low_points for lower bound of points to scrape, change filename and level for filename and level to scrape from
-high_points, low_points = 100, 95
-
-game = "goldeneye"
-level = "aztec"
-
-filename = "test2.csv"
 f = scraper.openfile("\\out\\" + filename)
 
 #open chrome browser at 'url'
 driver = scraper.openbrowser("https://rankings.the-elite.net/" + game + "/stage/" + level)
 
 #parse xml on rankspage and timespage to get personal best and date info
-scraper.parse_xml(f, driver, high_points, low_points)
+scraper.parse_xml(f, driver, high_points, low_points, select_a, select_sa, select_00a)
 
 driver.quit()
 f.close()
